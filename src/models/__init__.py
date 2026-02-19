2ee8d95fd6ed888d67c4281cee2ffe6a43da2a44
Alnur <alnur.tazhbenov@mail.ru>
2026-02-19 15:54:44 +0300

    feat: add records model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,9 @@
 from src.models.events import EventsOrm
+from src.models.records import RecordsOrm
 from src.models.users import UsersOrm
 
 __all__ = [
     "EventsOrm",
+    "RecordsOrm",
     "UsersOrm",
 ]
---
+++ src/models/records.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from src.database import Base
+
+
+class RecordsOrm(Base):
+    __tablename__ = "records"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    player_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    event_id: Mapped[int] = mapped_column(ForeignKey("events.id"))
+    datetime_from: Mapped[datetime]
+    datetime_to: Mapped[datetime]
